bb496dcea175894a1409e2bf4ad14970d7ab117d
Jonty <jonty@jonty.co.uk>
2010-04-24 17:01:50 +0100

    Changes from the live version of lighted running in the hackspace, bringing the repo up to date.

--- lighted.py
+++ lighted.py
@@ -30,18 +30,20 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         params = urlparse.parse_qs(url.query)
         path = url.path
 
-        bits = path.split('/')
-        bits.pop(0) # Crap
+        if path == '/':
+            path = '/resources/index.html'
+        elif path == '/favicon.ico':
+            path = '/resources/favicon.ico'
 
-        # Serve the root control page
-        if len(bits) == 1 and bits[0] == '':
-            bits = ['resources', 'index.html']
+        bits = path.split('/')
+        bits.pop(0) # Ignore initial slash
 
         # Serve the control page resources
         if len(bits) and bits[0] == 'resources':
+
             try:
-                f = open('/'.join(bits), 'rb')
-            except IOError:
+                f = open('%s/%s' % (bits[0], bits[1]), 'rb')
+            except IOError, e:
                 self.send_error(404, "File not found")
                 return
             
@@ -55,43 +57,47 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
         if len(bits) != 2:
             self.send_error(500)
             self.end_headers()
-            self.send_header('Content-Type', 'text/html')
             self.wfile.write('Bad request, should be in the format foo.com/device/r,g,b')
             return 
 
-        devices = []
+
         device = bits.pop(0) # Device
         if (device == '_'):
             devices = dmxDevices.keys()
         else:
-            devices = device.split(',')
-            devices = [int(x) for x in devices]
+            try:
+                devices = device.split(',')
+                devices = [int(x) for x in devices]
 
-        for dev in devices:
-            if dev not in dmxDevices:
+                for dev in devices:
+                    if dev not in dmxDevices:
+                        raise ValueError('Cannot find specified DMX device, available devices are: ')
+
+            except ValueError, e:
                 self.send_error(500)
                 self.end_headers()
-                self.send_header('Content-Type', 'text/html')
-                self.wfile.write('Cannot find specified DMX device, available devices are: ')
+                self.wfile.write(e)
                 self.wfile.write(', '.join(str(x) for x in dmxDevices.keys()))
                 return 
 
         colour = bits.pop(0) # Colour
-        if re.match('\d+,\d+,\d+', colour):
-            rgb = colour.split(',')
-        elif re.match('[0-9ABCDEF]{,6}', colour):
-            rgb = [int(x, 16) for x in (colour[0:2], colour[2:4], colour[4:6])]
+        try:
+          if re.match('\d+,\d+,\d+', colour):
+              rgb = colour.split(',')
+          elif re.match('[0-9ABCDEF]{,6}', colour):
+              rgb = [int(x, 16) for x in (colour[0:2], colour[2:4], colour[4:6])]
 
-        for val in rgb:
-            if int(val) < 0 or int(val) > 255:
+          for val in rgb:
+              if int(val) < 0 or int(val) > 255:
+                  raise ValueError('RGB value out of range, should be 0 <> 255')
+
+        except ValueError, e:
                 self.send_error(500)
                 self.end_headers()
-                self.send_header('Content-Type', 'text/html')
-                self.wfile.write('RGB value out of range, should be 0 <> 255')
+                self.wfile.write(e)
                 return 
 
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
         self.end_headers()
         self.wfile.write('OK')
         self.setColour(devices, rgb)
@@ -111,6 +117,6 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                 port.write("%sc%sw" % (dmxDevices[device] + i, rgb[i]))
         
 
-PORT = 8000
+PORT = config.getint('lighted', 'tcpport')
 httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
 httpd.serve_forever()
